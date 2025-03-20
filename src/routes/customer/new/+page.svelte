0c670e1889c4c16985aa9b6979422d05bc6439e6
João <107575192+jvmebe@users.noreply.github.com>
2025-03-19 22:15:37 -0300

    Implementar pag cliente

---
+++ src/routes/customer/new/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import {
+        Button,
+        Input,
+        FormGroup,
+        Form,
+        Table,
+    } from "@sveltestrap/sveltestrap";
+</script>
+
+<Form method="POST">
+    <FormGroup floating label="Nome">
+        <Input name="NAME" type="text" placeholder="Digite uma descrição" />
+    </FormGroup>
+    <FormGroup floating label="CPF/CNPJ">
+        <Input name="CPFCNPJ" type="number" placeholder="12" />
+    </FormGroup>
+    <FormGroup>
+        <Input name=""/>
+    </FormGroup>
+
+    <Button>Salvar</Button>
+</Form>
--- src/routes/paycondition/new/+page.svelte
+++ src/routes/paycondition/new/+page.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
-    import { Button, Input, FormGroup, Form } from "@sveltestrap/sveltestrap";
+    import {
+        Button,
+        Input,
+        FormGroup,
+        Form,
+        Table,
+    } from "@sveltestrap/sveltestrap";
 </script>
 
 <Form method="POST">
     <FormGroup floating label="Nome da Condição de Pagamento">
-        <Input name="DESCRIPTION" type="text" placeholder="Digite uma descrição" />
+        <Input
+            name="DESCRIPTION"
+            type="text"
+            placeholder="Digite uma descrição"
+        />
     </FormGroup>
-    <FormGroup floating label="Quantidade de Parcelas" >
+    <FormGroup floating label="Quantidade de Parcelas">
         <Input name="INSTALLMENT_QTY" type="number" placeholder="12" />
     </FormGroup>
 
-	
-	<Button>Salvar</Button>
+    <h2>Parcelas</h2>
+
+    <Table>
+        <thead>
+            <tr>
+                <th>N° Parcela</th>
+                <th>Prazo</th>
+                <th>Percentual</th>
+                <th>Forma Pagamento</th>
+            </tr>
+        </thead>
+        <tbody> </tbody>
+    </Table>
+
+    <Button>Salvar</Button>
 </Form>
